85adfb6eed229101229dd939fc4287fa0b2bfa8c
Utkarsh Anand <utkarsh.aanand@gmail.com>
2021-07-19 14:40:11 +0800

    Add class for drift correction.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import pandas
+import numpy
 
 sys.path.append(os.path.abspath('./lib'))
 import dataPrep
+import driftCorrection
 
 
 excelFile = 'analysisLog.xlsx'
@@ -13,10 +15,15 @@ df.dropna(subset=['inputDir'],inplace=True)
 
 for inputDir,fps,pixelSize,flux in df.values:
     dp = dataPrep.dataPrep(inputDir)
-    # dp.convertGatanMovie()
+    dp.convertGatanMovie()
     dp.averageFrames(dp.outputPNGDir,dp.outputPNGDir+'_avg',20,'png')
     dp.averageFrames(dp.outputNPYDir,dp.outputNPYDir+'_avg',10,'npy')
     dp.averageFrames(dp.outputPNGDir,dp.outputDir,dp.numFrames,'png')
+    
+    drift = driftCorrection.driftCorrection(inputDir,sigma=10)
+    drift.findDrift()
+    drift.smoothDrift()
+    drift.generateImages(numpy.load(drift.driftParamsSmoothFile),scale=0.25,radius=100)
     
     
     
