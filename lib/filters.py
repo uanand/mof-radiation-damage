8a9e78c4006f3b5b77e4fe265cbfeea639cee61d
Utkarsh Anand <utkarsh.aanand@gmail.com>
2021-08-07 00:04:00 +0800

    Retain only the two largest connected components in the binary image ro find the center of  diffraction pattern.

--- lib/filters.py
+++ lib/filters.py
@@ -1,15 +1,22 @@
 import imageProcess
 import numpy
+from skimage.measure import label, regionprops
 
 def areaFilter(bImg):
-    labelImg,numLabel,dictionary = imageProcess.regionProps(bImg)
+    labelImg = label(bImg)
+    numLabel = labelImg.max()
+    props = regionprops(labelImg)
     bImg[:] = False
     
-    regionAreaList = dictionary['area']
-    regionAreaList = sorted(regionAreaList,reverse=True)
-    regionAreaList = regionAreaList[:2]
-    for label,area in zip(range(1,numLabel+1),dictionary['area']):
-        if (area==regionAreaList[0] or area==regionAreaList[1]):
-            bImgN = labelImg==label
+    areaList = []
+    for i in range(numLabel):
+        areaList.append(props[i].area)
+    areaList = sorted(areaList,reverse=True)
+    areaList = areaList[:2]
+    
+    for i in range(numLabel):
+        area = props[i].area
+        if (area==areaList[0] or area==areaList[1]):
+            bImgN = labelImg==i+1
             bImg = numpy.logical_or(bImg,bImgN)
     return bImg
